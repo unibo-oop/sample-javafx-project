99ffa2de711a8122cc9f245c45d0e19bd48592f3
Danilo Pianini <danilo.pianini@unibo.it>
2024-12-02 09:09:17 +0100

    fix: let the base project point to the minimal example

--- src/main/java/it/unibo/samplejavafx/App.java
+++ src/main/java/it/unibo/samplejavafx/App.java
@@ -16,7 +16,7 @@ public final class App {
      * @param args
      */
     public static void main(final String[] args) {
-        Application.launch(JavaFXAppMoreStages.class, args);
+        Application.launch(JavaFXApp.class, args);
         // The following line raises: Error: class it.unibo.samplejavafx.App is not a subclass of javafx.application.Application
         // JavaFXApp.launch(args);
         // While the following would do just fine:
