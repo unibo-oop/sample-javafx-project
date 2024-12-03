973850ab2f791ba8d14b1800bdceb8f85a503050
Danilo Pianini <danilo.pianini@unibo.it>
2024-12-03 10:48:13 +0100

    ci: try to make JFX updates triggerable by renovate

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,11 +33,11 @@ dependencies {
     // Suppressions for SpotBugs
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.6")
 
-    // Example library: Guava. Add what you need (and remove Guava if you don't use it)
+    // Example library: Guava. Add what you need (and use the latest version where appropriate).
     // implementation("com.google.guava:guava:28.1-jre")
 
     // JavaFX: comment out if you do not need them
-    val javaFxVersion = 15
+    val javaFxVersion = "15"
     implementation("org.openjfx:javafx:$javaFxVersion")
     for (platform in supportedPlatforms) {
         for (module in javaFXModules) {
